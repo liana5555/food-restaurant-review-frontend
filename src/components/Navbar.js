c73cee7b63d3e216006e27f516c0433a76b07329
liana5555 <baloghildiko55@gmail.com>
2024-09-27 23:09:53 +0200

    Navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
 
 export default function Navbar() {
-  const [navBarStateClosed, setNavBarStateClosed] = React.useState(false);
+  const [navBarStateClosed, setNavBarStateClosed] = React.useState(true);
 
   const { currentUser, logout } = useContext(AuthContext);
 
-  const [profilemenu, setProfileMenu] = React.useState(false);
+  const [profileMenuIsOpen, setProfileMenu] = React.useState(false);
+
+  const path = useLocation();
+
+  console.log(path);
 
   function handlenavBarStateClosed() {
     setNavBarStateClosed((prev) => !prev);
@@ -20,103 +24,88 @@ export default function Navbar() {
   console.log(currentUser);
 
   return (
-    <nav className={navBarStateClosed ? "" : "mobile-open"}>
-      <div
-        onClick={handlenavBarStateClosed}
-        className={
-          navBarStateClosed ? "hamburger-menu" : "hamburger-menu-change"
-        }
-      >
-        <span className="hamburger-top-line"></span>
-        <span className="hamburger-middle-line"></span>
-        <span className="hamburger-bottom-line"></span>
-      </div>
-      <ul className={navBarStateClosed ? "ul_none" : "ul_visible"}>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        {currentUser && (
-          <li>
-            <Link to="/write">Write</Link>
+    <div>
+      <nav className={navBarStateClosed ? "" : "mobile-open"}>
+        <div
+          onClick={handlenavBarStateClosed}
+          className={
+            navBarStateClosed ? "hamburger-menu" : "hamburger-menu-change"
+          }
+        >
+          <span className="hamburger-top-line"></span>
+          <span className="hamburger-middle-line"></span>
+          <span className="hamburger-bottom-line"></span>
+        </div>
+
+        <ul className={navBarStateClosed ? "mobile-hidden" : ""}>
+          <li className={path.pathname === "/" ? "onPath" : ""}>
+            <Link to="/">Home</Link>
           </li>
-        )}
-        {currentUser && (
-          <li>
-            {" "}
-            <Link to="/chat">Chat</Link>
-          </li>
-        )}
-        <li>
-          <Link to="/restaurants">Restaurants</Link>
-        </li>
-        <li>
-          {currentUser ? (
-            <span className="link" onClick={logout}>
-              Logout
-            </span>
-          ) : (
-            <Link to="/login">Login/Sign up</Link>
+          {currentUser && (
+            <li className={path.pathname === "/write" ? "onPath" : ""}>
+              <Link to="/write">Write</Link>
+            </li>
           )}
-        </li>
-        {currentUser && (
-          <div className="nav-profile-container">
-            <div
-              className="user-img-name-container"
-              onMouseEnter={handleProfileMenu}
-              onClick={handleProfileMenu}
-            >
-              <img src={`../uploads/profile_pics/${currentUser?.img}`} />
-              <span className="link">{currentUser?.username}</span>
-            </div>
-            {profilemenu && (
-              <div
-                onMouseLeave={() => setProfileMenu(false)}
-                onMouseEnter={() => setProfileMenu(true)}
-                className={
-                  profilemenu
-                    ? "user-menu-container"
-                    : "user-menu-container-hidden"
-                }
-              >
-                <ul className="profile-sub-menu">
-                  <li>
-                    <Link to={`/user/${currentUser.idusers}`}>Profile</Link>
-                  </li>
-                  {currentUser.type === "admin" && (
-                    <li>
-                      <Link to="/admin/manage_users">Manage users</Link>
-                    </li>
-                  )}
-                  {currentUser.type === "admin" && (
-                    <li>
-                      <Link to="/admin/manage_reports">
-                        Manage reported posts
-                      </Link>
-                    </li>
-                  )}
-                  {currentUser.type === "restaurant worker" && (
-                    <li>
-                      <Link to="/restaurant_worker/manage_reservations">
-                        Manage reservations
-                      </Link>
-                    </li>
-                  )}
-                  {currentUser.type === "restaurant worker" && (
-                    <li>
-                      <Link to="/write/advertisement">Write Advertisement</Link>
-                    </li>
-                  )}
-                  <li>
-                    <a href={`/user/${currentUser.idusers}#delete-account`}>
-                      Delete account
-                    </a>
-                  </li>
-                </ul>
-              </div>
+          {currentUser && (
+            <li className={path.pathname === "/chat" ? "onPath" : ""}>
+              <Link to="/chat">Chat</Link>
+            </li>
+          )}
+          <li className={path.pathname === "/restaurants/" ? "onPath" : ""}>
+            <Link to="/restaurants/">Restaurants</Link>
+          </li>
+          <li>
+            {currentUser ? (
+              <span className="link" onClick={logout}>
+                Logout
+              </span>
+            ) : (
+              <Link to="/login">Login/Sign up</Link>
             )}
-          </div>
-        )}
-      </ul>
-    </nav>
+          </li>
+
+          {currentUser && (
+            <li>
+              <div
+                className="profile-img-name-container"
+                onClick={handleProfileMenu}
+              >
+                <img src={`../uploads/profile_pics/${currentUser?.img}`} />
+                <span className="link">{currentUser?.username}</span>
+              </div>
+            </li>
+          )}
+        </ul>
+      </nav>
+      {profileMenuIsOpen && (
+        <ul className="profile-menu" onMouseLeave={() => setProfileMenu(false)}>
+          <li>
+            <Link to={`/user/${currentUser.idusers}`}>Profile</Link>
+          </li>
+          {currentUser.type === "admin" && (
+            <li>
+              <Link to="/admin/manage_users">Manage users</Link>
+            </li>
+          )}
+          {currentUser.type === "admin" && (
+            <li>
+              <Link to="/admin/manage_reports">Manage reported posts</Link>
+            </li>
+          )}
+          {currentUser.type === "restaurant worker" && (
+            <li>
+              <Link to="/restaurant_worker/manage_reservations">
+                Manage reservations
+              </Link>
+            </li>
+          )}
+          {currentUser.type === "restaurant worker" && (
+            <li>
+              <Link to="/write/advertisement">Write Advertisement</Link>
+            </li>
+          )}
+        </ul>
+      )}
+    </div>
   );
 }
